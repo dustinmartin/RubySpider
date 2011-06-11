7948b4221feb9e4e6ed01063b8c6ff8d8e271e3b
Dustin Martin <dwmartin18@gmail.com>
2011-06-11 18:01:45 -0400

    Redesigned

--- crawler.rb
+++ crawler.rb
@@ -3,79 +3,99 @@ require 'open-uri'
 require 'uri'
 require_relative 'data'
 
+DO_NOT_CRAWL_TYPES = %w(.pdf .doc .xls .ppt .mp3 .m4v .avi .mpg .rss .xml .json .txt .git .zip .md5 .asc .jpg .gif .png)
+
 class Spider
 
-	def crawl url
+	# Seed DB with URL
+	def seed url
+		page = Page.new
+		page.url = url
+		page.save
+	end
 
-		if Page.count(:url => url) == 0
-			page = Page.new
-			page.url = url
-			page.indexed = false
-			page.domain =  get_domain(url)
-			page.save
-		else
-			page = Page.first(:url => url)
+	def start
+		domain = Page.first.domain
+
+		loop do
+			begin
+				page = Page.next domain
+
+				puts "Processing #{page.url}"
+				
+				crawl page
+
+				domain = page.domain
+			rescue => e 
+				puts "** Error encountered crawling - #{page.url} - #{e.to_s}"
+				exit
+			end
 		end
-		
-		if page.indexed == false
-			# Find and parse links
-			parse_links page
-
-			# Index page contents
-			index_page page
-		end
-
-		if Page.count(:domain => get_domain(url), :indexed => false)
-			next_page = Page.first(:domain => get_domain(url), :indexed => false).url
-		else
-			next_page = Page.first(:indexed => false).url	
-		end
-
-		crawl next_page
 	end
 
 	private
 
-	def get_domain url
-		URI.parse(url).host.gsub(/^www\./, '')
-	end
+	def crawl page
+		page.crawled_at = Time.now
 
-	def index_page page
-		page.indexed = true
+		parse page
+
 		page.save
-
-		doc = Nokogiri::HTML(open(page.url))
 	end
 
-	def parse_links page
-		doc = Nokogiri::HTML(open(page.url))
-	
-		doc.css('a').each do |link|
-			queue_link link['href']
+	def queue url
+		page = Page.find url
+		page.save
+	end
+
+	def parse page
+		# Get the URL for the current page
+		url = page.url
+
+		# Get the domain from the current page
+		domain = page.domain
+
+		begin
+			# Open the URL and parse
+			doc = Nokogiri::HTML(open(url))
+
+			# Loop through all the links and queue them up
+			doc.css('a').each do |link|
+				
+				# Verify that the link is valid
+				if link['href'] =~ URI::regexp
+					
+					# Check if the link is absolute
+					url = normalize(link['href'], domain)
+					
+					# Queue up the link
+					queue URI.escape(url)
+				end
+			end	
+		rescue => e
+			puts "** Error parsing links at url - #{page.url} - #{e.to_s}"
 		end
 	end
 
-	def queue_link url
-		unless (url =~ URI::regexp).nil?
-			# Need to check if url exists already
-			if Page.count(:url => url) == 0
-				page = Page.new
-				page.url = url
-				page.indexed = false
-				page.domain =  URI.parse(url).host.gsub(/^www\./, '')
-				page.save
-			end
+	def absolute? url
+		URI.parse(url).absolute?
+	end
+
+	def normalize url, domain
+		if not absolute? url
+			normalized_url = (URI.parse(domain) + url).to_s
+		else
+			normalized_url = url
 		end
+
+		normalized_url.sub(/(\/)+$/,'')
 	end
 
 end
 
 
-
-
-
 spider = Spider.new
 
-spider.crawl("http://www.dustinmartin.net/")
-#spider.list_urls
-#spider.list_domains
+spider.seed("http://en.wikipedia.org/wiki/Main_Page")
+spider.seed("http://www.dustinmartin.net")
+spider.start
--- data.rb
+++ data.rb
@@ -1,42 +1,70 @@
 require 'data_mapper'
 require  'dm-migrations'
 
-=begin
-class Domain
-	include DataMapper::Resource
-
-	property :id,			Serial
-	property :url,			String
-	property :created_at,	DateTime 
-	property :updated_at,	DateTime 	
-end
-=end
+TIMEOUT = 60 * 60 * 24 * 7 # 7 Days
 
 class Page
 	include DataMapper::Resource
 
 	property :id,			Serial
-	property :title,		String   
-	property :url,			String   
+	property :url,			String  
 	property :domain,		String
+
+	#property :crawl_count,	Integer
+
 	property :created_at,	DateTime
-	property :updated_at,	DateTime
-	property :indexed,		Boolean
+	property :crawled_at,	DateTime
 
-	has n, :words, :through => Resource
+	# Find a page by it's URL
+	def self.find url
+		page = first(:url => url)
+		page = new(:url => url) if page.nil?
+		return page
+	end
+
+	# Get the next crawlable Page
+	def self.next domain
+		past_date = Time.now - TIMEOUT
+		#domain = page.domain
+
+		page = first(:domain => domain, :crawled_at => nil) 
+		page = first(:domain => domain, :crawled_at.lte => date) if page.nil?
+		page = first(:crawled_at => nil) if page.nil?
+		page = first(:crawled_at.lte => date_range) if page.nil?
+		#page = Page.first() if page == nil
+
+		#page = Page.first(:crawled_at => nil) if page.nil?
+		#page = Page.first(:crawled_at.lte => past_date) if page.nil?
+		#page = Page.first() if page == nil
+
+		if page.nil?
+			throw "No page to crawl"
+		end
+
+		page
+	end
+
+	def url=(url)
+		attribute_set(:url, url)
+		attribute_set(:domain, get_domain(url))
+	end
+
+	private
+
+	def get_domain url
+		parsed_url = URI.parse(url)
+		scheme = parsed_url.scheme
+		domain = parsed_url.host.gsub(/^www\./, '')
+	
+		return "#{ scheme }://#{ domain }"
+	end
+
 end
 
-class Word
-	include DataMapper::Resource
-
-	property :id,			Serial
-	property :word,			String   
-	property :created_at,	DateTime 
-
-	has n, :pages, :through => Resource
-end
-
-DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, "sqlite:///#{Dir.pwd}/db/data.db")
+#DataMapper::Logger.new($stdout, :debug)
+#DataMapper.setup(:default, "sqlite:///#{Dir.pwd}/db/data.db")
+ 
+ # A Postgres connection:
+DataMapper.setup(:default, 'postgres://localhost/RubySpider')
 DataMapper.auto_migrate!
 DataMapper.finalize
